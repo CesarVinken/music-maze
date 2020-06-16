29ebe853ce1ba53562f508fcff5ee5304cd15cb1
CesarVinken <39000090+CesarVinken@users.noreply.github.com>
2020-06-16 16:11:45 +0200

    Display console button for Android platform

--- Assets/Script/UI/MainCanvas.cs
+++ Assets/Script/UI/MainCanvas.cs
@@ -7,11 +7,18 @@ public class MainCanvas : MonoBehaviour
     public static MainCanvas Instance;
 
     public GameObject ConsoleContainer;
+    public GameObject ConsoleButton;
 
     public void Awake()
     {
         Instance = this;
 
         Guard.CheckIsNull(ConsoleContainer, "ConsoleContainer");
+        Guard.CheckIsNull(ConsoleButton, "ConsoleButton");
+
+        if (GameManager.Instance.CurrentPlatform == Platform.Android)
+        {
+            ConsoleButton.SetActive(true);
+        }
     }
 }
