b6b1136e1db7d68d646d8c532ea9d52745724ebb
CesarVinken <39000090+CesarVinken@users.noreply.github.com>
2020-10-08 14:45:15 +0300

    Launcher cleanup

--- Assets/Scripts/Network/Launcher.cs
+++ Assets/Scripts/Network/Launcher.cs
@@ -1,34 +1,4 @@
-﻿/*
- * Copyright (c) 2019 Razeware LLC
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
- * distribute, sublicense, create a derivative work, and/or sell copies of the 
- * Software in any work that is designed, intended, or marketed for pedagogical or 
- * instructional purposes related to programming, coding, application development, 
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works, 
- * or sale is expressly withheld.
- *    
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 using Photon.Pun;
@@ -39,10 +9,10 @@ namespace Photon.Pun.Demo.PunBasics
     public class Launcher : MonoBehaviourPunCallbacks
     {
         [SerializeField]
-        private GameObject controlPanel;
+        private GameObject _controlPanel;
 
         [SerializeField]
-        private Text feedbackText;
+        private Text _feedbackText;
 
         [SerializeField]
         private byte maxPlayersPerRoom = 2;
@@ -53,44 +23,51 @@ namespace Photon.Pun.Demo.PunBasics
 
         [Space(10)]
         [Header("Custom Variables")]
-        public InputField playerNameField;
-        public InputField roomNameField;
+        [SerializeField] private InputField _playerNameField;
+        [SerializeField] private InputField _roomNameField;
 
         [Space(5)]
-        public Text playerStatus;
-        public Text connectionStatus;
+        [SerializeField] private Text _playerStatus;
+        [SerializeField] private Text _connectionStatus;
 
         [Space(5)]
-        public GameObject roomJoinUI;
-        public GameObject buttonLoadArena;
-        public GameObject buttonJoinRoom;
+        [SerializeField] private GameObject _roomJoinUI;
+        [SerializeField] private GameObject _loadArenaButtonGO;
+        [SerializeField] private GameObject _joinRoomButtonGO;
 
         string playerName = "";
         string roomName = "";
 
-        // Start Method
+        public void Awake()
+        {
+            if (_playerNameField == null)
+                Logger.Error(Logger.Initialisation, "Could not find _playerNameField component on Launcher");
+            if (_roomNameField == null)
+                Logger.Error(Logger.Initialisation, "Could not find _roomNameField component on Launcher");
+            if (_playerStatus == null)
+                Logger.Error(Logger.Initialisation, "Could not find _playerStatus component on Launcher");
+            if (_playerNameField == null)
+                Logger.Error(Logger.Initialisation, "Could not find _connectionStatus component on Launcher");
+
+            Guard.CheckIsNull(_roomJoinUI, "Could not find _roomJoinUI");
+            Guard.CheckIsNull(_loadArenaButtonGO, "Could not find _loadArenaButtonGO");
+            Guard.CheckIsNull(_joinRoomButtonGO, "Could not find _joinRoomButtonGO");
+
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
         void Start()
         {
-            //1
             PlayerPrefs.DeleteAll();
 
             Debug.Log("Connecting to Photon Network");
 
-            //2
-            roomJoinUI.SetActive(false);
-            buttonLoadArena.SetActive(false);
+            _roomJoinUI.SetActive(false);
+            _loadArenaButtonGO.SetActive(false);
 
-            //3
             ConnectToPhoton();
         }
 
-        void Awake()
-        {
-            //4 
-            PhotonNetwork.AutomaticallySyncScene = true;
-        }
-
-        // Helper Methods
         public void SetPlayerName(string name)
         {
             playerName = name;
@@ -101,71 +78,64 @@ namespace Photon.Pun.Demo.PunBasics
             roomName = name;
         }
 
-        // Tutorial Methods
         void ConnectToPhoton()
         {
-            connectionStatus.text = "Connecting...";
-            PhotonNetwork.GameVersion = gameVersion; //1
-            PhotonNetwork.ConnectUsingSettings(); //2
+            _connectionStatus.text = "Connecting...";
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
         }
 
         public void JoinRoom()
         {
             if (PhotonNetwork.IsConnected)
             {
-                PhotonNetwork.LocalPlayer.NickName = playerName; //1
-                Debug.Log("PhotonNetwork.IsConnected! | Trying to Create/Join Room " + roomNameField.text);
-                RoomOptions roomOptions = new RoomOptions(); //2
-                TypedLobby typedLobby = new TypedLobby(roomName, LobbyType.Default); //3
-                PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, typedLobby); //4
+                PhotonNetwork.LocalPlayer.NickName = playerName;
+                Debug.Log("PhotonNetwork.IsConnected! | Trying to Create/Join Room " + _roomNameField.text);
+                RoomOptions roomOptions = new RoomOptions();
+                TypedLobby typedLobby = new TypedLobby(roomName, LobbyType.Default);
+                PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, typedLobby);
             }
         }
 
         public void LoadArena()
         {
-            // 5
             if (PhotonNetwork.CurrentRoom.PlayerCount > 1)
             {
                 PhotonNetwork.LoadLevel("SampleScene");
             }
             else
             {
-                playerStatus.text = "Minimum 2 Players required to Load Arena!";
+                _playerStatus.text = "Minimum 2 Players required to Load Arena!";
             }
         }
 
-        // Photon Methods
         public override void OnConnected()
         {
-            // 1
             base.OnConnected();
-            // 2
-            connectionStatus.text = "Connected to Photon!";
-            connectionStatus.color = Color.green;
-            roomJoinUI.SetActive(true);
-            buttonLoadArena.SetActive(false);
+            _connectionStatus.text = "Connected to Photon!";
+            _connectionStatus.color = Color.green;
+            _roomJoinUI.SetActive(true);
+            _loadArenaButtonGO.SetActive(false);
         }
 
         public override void OnDisconnected(DisconnectCause cause)
         {
-            // 3
             isConnecting = false;
-            controlPanel.SetActive(true);
+            _controlPanel.SetActive(true);
             Debug.LogError("Disconnected. Please check your Internet connection.");
         }
 
         public override void OnJoinedRoom()
         {
-            // 4
             if (PhotonNetwork.IsMasterClient)
             {
-                buttonLoadArena.SetActive(true);
-                buttonJoinRoom.SetActive(false);
-                playerStatus.text = "Your are Lobby Leader";
+                _loadArenaButtonGO.SetActive(true);
+                _joinRoomButtonGO.SetActive(false);
+                _playerStatus.text = "Your are Lobby Leader";
             }
             else
             {
-                playerStatus.text = "Connected to Lobby";
+                _playerStatus.text = "Connected to Lobby";
             }
         }
     }
